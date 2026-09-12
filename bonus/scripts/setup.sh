1d04c33399ff5ebbb1d069d06b1b96a6fca175e8
Ayoub Chraiti <ayoubchraiti2@gmail.com>
2026-09-12 21:47:41 +0100

    ..

--- bonus/scripts/setup.sh
+++ bonus/scripts/setup.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
-set -euo pipefail
-
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)
+set -e
 
 kubectl config use-context k3d-mycluster
-kubectl create namespace gitlab --dry-run=client -o yaml | kubectl apply -f -
-
-kubectl apply -f "$SCRIPT_DIR/../gitlab.yaml"
+kubectl apply -f "../gitlab.yaml"
 kubectl -n gitlab rollout status statefulset/gitlab --timeout=1800s
